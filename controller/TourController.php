046d6581e8e1455d5adb17236d616bdc9ada5f0a
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-06-06 15:37:48 -0300

    busqueda tours

--- controller/TourController.php
+++ controller/TourController.php
@@ -21,4 +21,19 @@ class TourController
 
         $this->printer->generateView('tourView.html', $data);
     }
+
+    public function busqueda(){
+        if (isset($_SESSION["AdminIn"]) || isset($_SESSION["ClienIn"])) {
+            $data["loggeado"] = 1;
+            $data["nombre"] = $_SESSION["usuario"];
+        }
+
+        $dia = $_POST["dia"] ?? "";
+        $origen = $_POST["origen"] ?? "";
+
+        $respuesta = $this->tourModel->getTours($dia, $origen);
+        $data["tours"] = $respuesta;
+
+        $this->printer->generateView('tourView.html', $data);
+    }
 }
