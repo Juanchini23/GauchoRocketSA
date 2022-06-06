2abf87ac887d157fac7c8c9a218c59108dbf46ce
TomiiVaz <eliseovazquez01@gmail.com>
2022-06-06 16:17:39 -0300

    Tour and Orbital
    Co-Authored-By: Damian Nahuel Mitria <89418934+damian-mitria@users.noreply.github.com>
    Co-Authored-By: Juanchini23 <91612292+Juanchini23@users.noreply.github.com>

--- model/TourModel.php
+++ model/TourModel.php
@@ -12,28 +12,26 @@ class TourModel
 
     public function getTours($dia, $origen)
     {
-        {
-            if (strlen($dia) == null || strlen($origen) == null) {
-                return $this->dataBase->query("SELECT p.id, p.dia as 'dia', p.horaPartida as 'hora', o.descripcion as 'origen', n.modelo as 'modelo'
+        if (strlen($dia) == null || strlen($origen) == null) {
+            return $this->dataBase->query("SELECT p.id, p.dia as 'dia', p.horaPartida as 'hora', o.descripcion as 'origen', n.modelo as 'modelo'
 FROM planificacion p
          JOIN origen o ON p.idOrigen = o.id
          JOIN modelo m ON p.idModelo = m.id
          JOIN nave n ON m.idNave = n.id
          JOIN tipoVuelo tv ON tv.id = p.idTipoVuelo
-WHERE o.descripcion = '$origen'
-OR p.dia = '$dia'
-AND tv.descripcion = 'Orbitales'");
-            } else {
-                return $this->dataBase->query("SELECT p.id, p.dia as 'dia', p.horaPartida as 'hora', o.descripcion as 'origen', n.modelo as 'modelo'
+WHERE (o.descripcion = '$origen'
+OR p.dia = '$dia')
+AND tv.descripcion = 'Tour'");
+        } else {
+            return $this->dataBase->query("SELECT p.id, p.dia as 'dia', p.horaPartida as 'hora', o.descripcion as 'origen', n.modelo as 'modelo'
 FROM planificacion p
          JOIN origen o ON p.idOrigen = o.id
          JOIN modelo m ON p.idModelo = m.id
          JOIN nave n ON m.idNave = n.id
          JOIN tipoVuelo tv ON tv.id = p.idTipoVuelo
-WHERE o.descripcion = '$origen'
-AND p.dia = '$dia'
-AND tv.descripcion = 'Orbitales'");
-            }
+WHERE (o.descripcion = '$origen'
+AND p.dia = '$dia')
+AND tv.descripcion = 'Tour'");
         }
     }
 }
