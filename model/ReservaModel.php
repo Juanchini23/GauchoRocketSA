5735263bb87e3d78745065bab9fad304513ed2f3
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-06-26 00:28:37 -0300

    logica reserva internos

--- model/ReservaModel.php
+++ model/ReservaModel.php
@@ -5,10 +5,10 @@ class ReservaModel
 
     private $dataBase;
     private $cero = 0;
-    private $circuitoUnoBA = array(["Tierra" => 0, "EEI" => 4, "HotelOrbital" => 8, "Luna" => 16, "Marte" => 26]);
-    private $circuitoUnoAA = array(["Tierra" => 0, "EEI" => 3, "HotelOrbital" => 6, "Luna" => 9, "Marte" => 22]);
-    private $circuitoDosBA = array(["Tierra" => 0, "EEI" => 4, "Luna" => 14, "Marte" => 26, "Ganimedes" => 48, "Europa" => 50, "Io" => 51, "Encedalo" => 70, "Titan" => 77]);
-    private $circuitoDosAA = array(["Tierra" => 0, "EEI" => 3, "Luna" => 10, "Marte" => 22, "Ganimedes" => 32, "Europa" => 33, "Io" => 35, "Encedalo" => 50, "Titan" => 52]);
+    private $circuitoUnoBA = array(["Tierra"=> 0, "EEI" => 4, "HotelOrbital" => 8, "Luna" => 16, "Marte" => 26]);
+    private $circuitoUnoAA = array(["Tierra"=> 0, "EEI" => 3, "HotelOrbital" => 6, "Luna" => 9, "Marte" => 22]);
+    private $circuitoDosBA = array(["Tierra"=> 0, "EEI" => 4, "Luna" => 14, "Marte" => 26, "Ganimedes" => 48, "Europa" => 50, "Io" => 51, "Encedalo" => 70, "Titan" => 77]);
+    private $circuitoDosAA = array(["Tierra"=> 0, "EEI" => 3, "Luna" => 10, "Marte" => 22, "Ganimedes" => 32, "Europa" => 33, "Io" => 35, "Encedalo" => 50, "Titan" => 52]);
 
 
     /*let circuitoUnoBA = {"Tierra": 0, "EEI": 4, "HotelOrbital": 8, "Luna": 16, "Marte": 26};
@@ -47,58 +47,82 @@ class ReservaModel
     public function generarReserva($origen, $destino, $diaSalida, $horaSalida, $butaca, $cantidadAsientos, $metodoPago, $idUser, $idPlanificacion, $fecha)
     {
         $planificacion = $this->dataBase->getPlani($idPlanificacion);
-        $origenID='';
+        $origenID = '';
         $destinoID = '';
-        switch ($origen){
-            case 'BA': $origenID = 1;
-            break;
-            case 'AK': $origenID = 2;
+        switch ($origen) {
+            case 'BA':
+                $origenID = 1;
                 break;
-            case 'EEI': $origenID = 11;
+            case 'AK':
+                $origenID = 2;
                 break;
-            case 'HotelOrbital': $origenID = 10;
+            case 'EEI':
+                $origenID = 11;
                 break;
-            case 'Luna': $origenID = 9;
+            case 'HotelOrbital':
+                $origenID = 10;
                 break;
-            case 'Marte': $origenID = 8;
+            case 'Luna':
+                $origenID = 9;
                 break;
-            case 'Ganimedes': $origenID = 7;
+            case 'Marte':
+                $origenID = 8;
                 break;
-            case 'Europa': $origenID = 6;
+            case 'Ganimedes':
+                $origenID = 7;
                 break;
-            case 'Io': $origenID = 5;
+            case 'Europa':
+                $origenID = 6;
                 break;
-            case 'Encedalo': $origenID = 4;
+            case 'Io':
+                $origenID = 5;
                 break;
-            case 'Titan': $origenID = 3;
+            case 'Encedalo':
+                $origenID = 4;
                 break;
-            default: $origenID =0;
+            case 'Titan':
+                $origenID = 3;
+                break;
+            default:
+                $origenID = 0;
         }
 
-        switch ($destino){
-            case 'BA': $destinoID = 1;
+        switch ($destino) {
+            case 'BA':
+                $destinoID = 1;
                 break;
-            case 'AK': $destinoID = 2;
+            case 'AK':
+                $destinoID = 2;
                 break;
-            case 'EEI': $destinoID = 11;
+            case 'EEI':
+                $destinoID = 11;
                 break;
-            case 'HotelOrbital': $destinoID = 10;
+            case 'HotelOrbital':
+                $destinoID = 10;
                 break;
-            case 'Luna': $destinoID = 9;
+            case 'Luna':
+                $destinoID = 9;
                 break;
-            case 'Marte': $destinoID = 8;
+            case 'Marte':
+                $destinoID = 8;
                 break;
-            case 'Ganimedes': $destinoID = 7;
+            case 'Ganimedes':
+                $destinoID = 7;
                 break;
-            case 'Europa': $destinoID = 6;
+            case 'Europa':
+                $destinoID = 6;
                 break;
-            case 'Io': $destinoID = 5;
+            case 'Io':
+                $destinoID = 5;
                 break;
-            case 'Encedalo': $destinoID = 4;
+            case 'Encedalo':
+                $destinoID = 4;
                 break;
-            case 'Titan': $destinoID = 3;
+            case 'Titan':
+                $destinoID = 3;
                 break;
-            default: $destinoID =0;
+            default:
+                $destinoID = 0;
         }
 
         /*// 1- ver si existe una reserva con el id de la planificacion
@@ -109,6 +133,31 @@ class ReservaModel
                                             AND r.destino = '$destino';");
         $idReserva = $existe['id'];*/
         /*$contadorErrores = 0;*/
+
+        //////////////////// Logica para reservar  ////////////////////////
+        // para obtener la key del array o el array de keys
+        $keyCircuitoUnoBA = array_keys($this->circuitoUnoBA[0]);
+
+        $entro = false;
+        if($origen=='AK' || $origen=='BA'){
+            $keyCircuitoUnoBA[0]='Tierra';
+        }
+        $origen = 'EEI';
+        $destino = 'Luna';
+        $i=-1;
+        do{
+            $i++;
+            if ($keyCircuitoUnoBA[$i] == $origen) {
+                $entro = true;
+            }
+            if ($entro == true) {
+                echo($keyCircuitoUnoBA[$i]);
+                echo '+';
+                echo($keyCircuitoUnoBA[$i+1]);
+                echo '<br>';
+            }
+        }while($keyCircuitoUnoBA[$i+1]!=$destino);
+        /////////////////////////////////////////////////////////////////////
 
         // Para saber la cantidad maxima de butacas que tiene el modelo de nave.
         $cantidadMaximaButacaSeleccionada = $this->dataBase->query("SELECT $butaca as 'butaca' FROM planificacion p JOIN modelo m ON p.idModelo = m.id
@@ -123,31 +172,30 @@ class ReservaModel
 
         $cantidadA = $cantidadActualButacaReservadas[0]['cantidad'];
 
-        $sumaAsientos = $cantidadA+$cantidadAsientos;
+        $sumaAsientos = $cantidadA + $cantidadAsientos;
 
-            // verificar la cantidad de asientos y la clase
-            if ($sumaAsientos >= $cantidadM) {
-                $_SESSION['errorNoHayAciento']=1;
-                header("location:/");
-                exit();
-            } else{
-                if ($butaca == 'turista') {
-                    $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva) 
-                                                values('$cantidadAsientos',0,0,'$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
-                } elseif ($butaca == 'ejecutiva') {
-                    $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva) 
-                                                values(0,'$cantidadAsientos',0,'$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
-                } elseif ($butaca == 'primera') {
-                    $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva) 
-                                                values(0,0,'$cantidadAsientos','$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
-                }
+        // verificar la cantidad de asientos y la clase
+        /*if ($sumaAsientos >= $cantidadM) {
+            $_SESSION['errorNoHayAciento']=1;
+            header("location:/");
+            exit();
+        } else{
+            if ($butaca == 'turista') {
+                $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva)
+                                            values('$cantidadAsientos',0,0,'$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
+            } elseif ($butaca == 'ejecutiva') {
+                $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva)
+                                            values(0,'$cantidadAsientos',0,'$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
+            } elseif ($butaca == 'primera') {
+                $this->dataBase->reservar("insert into reserva(turista, ejecutivo, primera , idUsuario, idPlanificacion, fecha, idOrigenReserva, idDestinoReserva)
+                                            values(0,0,'$cantidadAsientos','$idUser','$idPlanificacion','$fecha','$origenID','$destinoID');");
             }
+        }*/
 
 
-
-            //        Calcularlo
-            //$diaLlegada = $_POST["diaLlegada"] ?? "";
-            //$horaLlegada = $_POST["horaLlegada"] ?? "";
+        //        Calcularlo
+        //$diaLlegada = $_POST["diaLlegada"] ?? "";
+        //$horaLlegada = $_POST["horaLlegada"] ?? "";
 
     }
 }
